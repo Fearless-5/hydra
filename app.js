d1a0fb13593c95b0d6667c9b53f41e13e3a62cfe
Fearless-5 <160542986+Fearless-5@users.noreply.github.com>
2024-12-05 14:39:12 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+import express from 'express';
+const serve = express();
+serve.use(express.json());
+serve.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
+serve.use(express.urlencoded({extended: true}));
+import { initializeApp } from "firebase/app";
+import { getDatabase, set, ref, onValue, child, push} from "firebase/database";
+const firebaseConfig = {
+  apiKey: "AIzaSyCZuRGTIWQpEgaG9a-40bMtfmoautz_qbk",
+  authDomain: "lynx2server.firebaseapp.com",
+  databaseURL: "https://lynx2server-default-rtdb.firebaseio.com",
+  projectId: "lynx2server",
+  storageBucket: "lynx2server.firebasestorage.app",
+  messagingSenderId: "711323925875",
+  appId: "1:711323925875:web:eeeaa70395ab4431385906"
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const url = ref(database, "/");
+serve.get("/api", (req, res) => {
+  onValue(url, (s) => {
+     res.json(s.val());
+  })
+})
+serve.post("/api", (req, res) => {
+  const { webRef, data } = req.body;
+  const id = push(child(ref(database, webRef), webRef)).key;
+  res.json({"id": id});
+})
+serve.post("/api/v1", (req, res) => {
+  const { webRef, data } = req.body;
+  set(ref(database, "/"+webRef), data);
+  const eurl = ref(database, webRef);
+  onValue(eurl, (s) => {
+     res.json(s.val());
+  })
+})
+serve.post("/api/v2", (req, res) => {
+  const { Ref, data, id } = req.body;
+  set(ref(database, `${Ref}/${id}`), data);
+  res.send("Server api v2 200!");
+})
+serve.listen(3000, () => {
+  console.log("Server is running!");
+})
